8548539bfc7735e54157cac76bc6a34ab71a378e
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-26 19:52:16 -0300

    Add Route to list all available tickets

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateEventControler } from "./controllers/CreateEventController";
 import { CreateHostEventController } from "./controllers/CreateHostEventController";
 import { CreateTicketController } from "./controllers/CreateTicketController";
+import { ListAllAvailableTicketsController } from "./controllers/ListAllAvailableTicketsController";
 import { ListAllTicketsController } from "./controllers/ListAllTicketsController";
 
 const routes = Router();
@@ -10,5 +11,6 @@ routes.post("/hostevent", new CreateHostEventController().handle);
 routes.post("/event", new CreateEventControler().handle);
 routes.post("/ticket", new CreateTicketController().handle);
 routes.get("/ticket", new ListAllTicketsController().handle);
+routes.get("/availabletickets", new ListAllAvailableTicketsController().handle);
 
 export { routes };
