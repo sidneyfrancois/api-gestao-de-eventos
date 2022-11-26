44594f8bcd65b31b5e527347f4ea99a027433b2f
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-26 18:29:56 -0300

    Add route to create Event

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { CreateEventControler } from "./controllers/CreateEventController";
 import { CreateHostEventController } from "./controllers/CreateHostEventController";
 
 const routes = Router();
 
 routes.post("/hostevent", new CreateHostEventController().handle);
+routes.post("/event", new CreateEventControler().handle);
 
 export { routes };
