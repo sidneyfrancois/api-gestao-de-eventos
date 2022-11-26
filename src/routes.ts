4f06f74f5f7357fc2d2561601e06423e172b0f17
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-26 18:40:03 -0300

    Add route to create ticket

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { CreateEventControler } from "./controllers/CreateEventController";
 import { CreateHostEventController } from "./controllers/CreateHostEventController";
+import { CreateTicketController } from "./controllers/CreateTicketController";
 
 const routes = Router();
 
 routes.post("/hostevent", new CreateHostEventController().handle);
 routes.post("/event", new CreateEventControler().handle);
+routes.post("/ticket", new CreateTicketController().handle);
 
 export { routes };
