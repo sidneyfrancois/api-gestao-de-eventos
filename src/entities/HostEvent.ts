57b9e51082464dfddf623f188b5a96e63e2efa45
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-26 18:07:27 -0300

    Add decorators and references to FK in Event entity

--- src/entities/HostEvent.ts
+++ src/entities/HostEvent.ts
@@ -1,9 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 import { Event } from "./Event";
 
+@Entity("hostsevent")
 class HostEvent {
+  @PrimaryColumn()
   id: string;
+
+  @OneToMany(() => Event, (event) => event.hostEvent)
   events: Event[];
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
