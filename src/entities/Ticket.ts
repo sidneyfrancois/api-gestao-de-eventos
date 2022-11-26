f8cdca0f8a64590ee1198e95e9d389111122d935
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-26 18:07:49 -0300

    Add decorators and references to FK in Ticket entity

--- src/entities/Ticket.ts
+++ src/entities/Ticket.ts
@@ -1,9 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 import { Event } from "./Event";
 
+@Entity("tickets")
 class Ticket {
+  @PrimaryColumn()
   id: string;
+
+  @ManyToOne(() => Event)
+  @JoinColumn({ name: "event_id" })
   event: Event;
+
+  @Column()
+  event_id: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
