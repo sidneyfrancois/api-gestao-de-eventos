0b8f2ad92c1c0906a9181cc87ec9f4e420e089d7
Sidney François <sidney.fvm.dev@outlook.com>
2022-11-28 17:32:40 -0300

    Add Error handling on server to catch error of class type AppError

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
 require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
 import { AppDataSource } from "./database";
 import { routes } from "./routes";
-import express from "express";
+import { AppError } from "./error/AppError";
+import "express-async-errors";
 
 const app = express();
 
@@ -10,6 +12,26 @@ AppDataSource.initialize()
   .then(() => {
     app.use(express.json());
     app.use(routes);
+
+    app.use(
+      (
+        err: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction
+      ) => {
+        if (err instanceof AppError) {
+          return response.status(err.statusCode).json({
+            message: err.message,
+          });
+        }
+
+        return response.status(500).json({
+          status: "error",
+          message: `Internal server error - ${err.message}`,
+        });
+      }
+    );
 
     app.listen(process.env.PORT, () => console.log("Event API is running"));
   })
